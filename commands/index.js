08d184e1b507b214263f16d6ef00e87d9db943e0
goliatone <burgosemi@gmail.com>
2017-10-29 17:26:46 -0400

    use new attach routine

--- commands/index.js
+++ commands/index.js
@@ -2,8 +2,18 @@
 
 const Collect = require('./collect');
 const Generate = require('./generate');
+/**
+ * Attach commands to given application context,
+ * if a `namespace` is given then commands will 
+ * be added as sub-commands.
+ */
+module.exports.attach = function $attach(app, namespace=false) {
+    
+    const context = {
+        namespace,
+        prog: app.prog
+    };
 
-module.exports.attach = function(prog, namespace=false) {
-    Collect.attach(prog, namespace);
-    Generate.attach(prog, namespace);
+    Collect.attach(context);
+    Generate.attach(context);
 };
