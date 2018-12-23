013175c7e9a71f9833e6726f4f6c6aeb21cc8936
goliatone <burgosemi@gmail.com>
2018-12-23 16:34:02 +0100

    add jsonschema command

--- commands/generate.js
+++ commands/generate.js
@@ -18,36 +18,49 @@ class GenerateCommand extends BaseCommand {
         event.source = resolve(event.source);
         event.output = resolve(event.output);
 
-        return this.loadSchema(event.source).then((models) => {
+        this.logger.info('Look for files at: %s', event.source);
+
+        return this.loadSchema(event.source).then(models => {
             return new Promise((resolve, reject) => {
                 let output;
                 try {
                     output = Schema(models, event.options);
-                } catch(e){
+                } catch (e) {
                     return reject(e);
                 }
                 return this.serializeOutput(event.output, output);
             });
-        }).catch((err)=> {
+        }).catch((err) => {
+            this.logger.error('Error running script');
+            this.logger.error(err.message);
             this.logger.error(err);
             return err;
         });
     }
-    //@TODO Make BaseCommand.loadJSON
+
+    /**
+     * @TODO Make BaseCommand.loadJSON
+     * @param {String} filepath Filepath
+     */
     loadSchema(filepath) {
         return new Promise((resolve, reject) => {
             readFile(filepath, 'utf-8', (err, content) => {
-                if(err) reject(err);
+                if (err) reject(err);
                 try {
                     let models = JSON.parse(content);
                     resolve(models);
-                } catch(e) {
+                } catch (e) {
                     reject(e);
                 }
             });
         });
     }
-    //@TODO Make BaseCommand.saveJSON
+
+    /**
+     * @TODO Make BaseCommand.saveJSON
+     * @param {String} filename 
+     * @param {String} output 
+     */
     serializeOutput(filename, output) {
         return new Promise((resolve, reject) => {
             try {
@@ -55,27 +68,27 @@ class GenerateCommand extends BaseCommand {
             } catch (e) {
                 return reject(e);
             }
-            writeFile(filename, output, (err)=> {
-                if(err) return reject(err);
+            writeFile(filename, output, (err) => {
+                if (err) return reject(err);
                 resolve(output);
             });
         });
     }
 
-    static describe(prog, cmd){
-        cmd.argument('[source]', 
-            'Path to directory with models', 
-            /.*/, 
+    static describe(prog, cmd) {
+        cmd.argument('[source]',
+            'Path to directory with models',
+            /.*/,
             GenerateCommand.DEFAULTS.source
         );
-        
-        cmd.argument('[output]', 
-            'Filename for output.', 
-            /.*/, 
+
+        cmd.argument('[output]',
+            'Filename for output.',
+            /.*/,
             GenerateCommand.DEFAULTS.output
         );
-        
-        cmd.option('--uri-prefix <prefix>', 
+
+        cmd.option('--uri-prefix <prefix>',
             'Add <prefix> to all element ids'
         );
     }
